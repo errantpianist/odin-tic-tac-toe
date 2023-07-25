2f9cedb9d9aba584b8dc0b176f54bf3e625c3ca1
Yu Zhou Zhang <yuzspanish@gmail.com>
2023-07-25 21:41:58 +0100

    add xo marker click handler

--- script.js
+++ script.js
@@ -25,10 +25,32 @@ const displayController = (() => {
       square.classList.add("square");
       square.setAttribute("data-index", index);
       square.textContent = element;
+      square.addEventListener("click", (e) => {
+        gameController.play(e.target.dataset.index);
+        renderBoard(Gameboard.getBoard());
+      });
       boardDiv.appendChild(square);
     });
   };
   return { renderBoard };
+})();
+
+const gameController = (() => {
+  const player1 = Player("Player 1", "X");
+  const player2 = Player("Player 2", "O");
+  let currentPlayer = player1;
+  const getCurrentPlayer = () => currentPlayer;
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+  const play = (index) => {
+    Gameboard.setBoard(index, currentPlayer.getMarker());
+    switchPlayer();
+  };
+  const reset = () => {
+    Gameboard.resetBoard();
+  };
+  return { getCurrentPlayer, switchPlayer, play, reset };
 })();
 
 displayController.renderBoard(Gameboard.getBoard());
