b9afbaa70337e5fe0f7c2108fafae8a7df76eaa7
Yu Zhou Zhang <yuzspanish@gmail.com>
2023-07-25 22:28:06 +0100

    add player name input modals

--- script.js
+++ script.js
@@ -51,7 +51,53 @@ const gameController = (() => {
   const reset = () => {
     Gameboard.resetBoard();
   };
-  return { getCurrentPlayer, switchPlayer, play, reset };
+  const checkWinner = () => {
+    const board = Gameboard.getBoard();
+    const winningCombos = [
+      [0, 1, 2], // top row
+      [3, 4, 5], // middle row
+      [6, 7, 8], // bottom row
+      [0, 3, 6], // left column
+      [1, 4, 7], // middle column
+      [2, 5, 8], // right column
+      [0, 4, 8], // left diagonal
+      [2, 4, 6], // right diagonal
+    ];
+    winningCombos.forEach((combo) => {
+      if (
+        board[combo[0]] !== "" &&
+        board[combo[0]] === board[combo[1]] &&
+        board[combo[1]] === board[combo[2]]
+      ) {
+        return board[combo[0]];
+      } else if (!board.includes("")) {
+        return "tie";
+      }
+    });
+    return null;
+  };
+
+  return { getCurrentPlayer, switchPlayer, play, reset, checkWinner };
 })();
+
+const playerInputs = document.querySelectorAll(".player-names input");
+playerInputs.forEach((input) => {
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      if (input.value !== "") {
+        e.target.parentElement.classList = "modal hidden";
+        e.target.parentElement.parentElement.querySelector("h2").textContent +=
+          e.target.value;
+        e.target.parentElement.parentElement
+          .querySelector("h2")
+          .classList.remove("hidden");
+        if (e.target.id === "player1") {
+          document.querySelector("#player2").focus();
+        }
+      }
+    }
+  });
+});
 
 displayController.renderBoard(Gameboard.getBoard());
